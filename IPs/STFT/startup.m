648a24dfe5f62494b18f4ad7b208c7300d98d8d4
Kandor9178 <andrew.s.law@gmail.com>
2025-02-05 14:08:51 +0000

    Forgot to add startup

---
+++ IPs/STFT/startup.m
@@ -0,0 +1,15 @@
+% Clock domain
+fs = 16e6;
+
+% Test tone
+f = 0.2*16e6;
+
+% Parameters
+N = 2048;
+n = 0:N-1;
+
+% Windows
+Hamming = 0.54 - 0.46 * cos(2*pi*n/N);
+Norm_Ham = sum(Hamming);
+Hanning = 0.5 * (1 + cos(2*pi*n/N));
+Norm_Han = sum(Hanning);
